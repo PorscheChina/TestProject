14e2242afc6cf4203990f2639579a27b6eb6c13f
你的名字 <>
2016-06-28 11:09:55 +0000

    版本1
    打法的身份

---
+++ TestProject/TestProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestProject
+//
+//  Created by shenyan on 16/6/28.
+//  Copyright © 2016年 shenyan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
